89a775ad04e5da652c5b36908edb3a7913fba9b8
zcq8652 <62883494+zcq8652@users.noreply.github.com>
2020-11-24 14:02:17 +0800

    springboot_OA_前端

---
+++ springboot_OA_前端/springboot_oa/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Header/>
+    <Content/>
+  </div>
+</template>
+
+<script>
+import Header from "./components/header";
+import Content from "./components/content";
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Content
+  }
+}
+</script>
+
+<style>
+#app {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ springboot_OA_前端/springboot_oa/src/components/header.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+       <ul>
+           <li class="title">职工人事管理系统</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+           
+        }
+    }
+}
+</script>
+
+<style scoped>
+.container{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 50px;
+    border: 1px solid #263f52;
+    background-color: #263f52;
+    z-index: 100;
+}
+.title{
+    margin-top: -17px;
+    margin-left: 20px;
+    width: 150px;
+    height: 50px;
+    /*border: 1px solid #fff;*/
+    font-size: 18px;
+    color: #fff;
+    list-style: none;
+    line-height: 50px;
+    font-weight: bold;
+}
+</style>
